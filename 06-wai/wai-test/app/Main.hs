4287cb1874928298a619aa3fe38f720941cb0b38
Goryudyuma <umaretekyoumade@gmail.com>
2019-10-09 00:13:01 +0900

    Hello, Web

--- 06-wai/wai-test/app/Main.hs
+++ 06-wai/wai-test/app/Main.hs
@@ -1,6 +1,14 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import           Lib
+import           Network.Wai                    ( Application
+                                                , responseLBS
+                                                )
+import           Network.HTTP.Types             ( ok200 )
+import           Network.Wai.Handler.Warp       ( run )
+
+app :: Application
+app _ respond =
+    respond $ responseLBS ok200 [("Content-Type", "text/plain")] "Hello, Web!"
 
 main :: IO ()
-main = someFunc
+main = run 8080 app
