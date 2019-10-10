b601d0467520f9d0e1728411bcd26cf87815ffef
Goryudyuma <umaretekyoumade@gmail.com>
2019-10-10 23:18:56 +0900

    add routing

--- 06-wai/wai-test/app/Main.hs
+++ 06-wai/wai-test/app/Main.hs
@@ -1,14 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Network.Wai                    ( Application
-                                                , responseLBS
-                                                )
-import           Network.HTTP.Types             ( ok200 )
-import           Network.Wai.Handler.Warp       ( run )
+import Data.ByteString.Lazy (fromStrict)
+import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8)
+import Network.HTTP.Types (notFound404, ok200)
+import Network.Wai (Application, pathInfo, responseLBS)
+import Network.Wai.Handler.Warp (run)
 
 app :: Application
-app _ respond =
-    respond $ responseLBS ok200 [("Content-Type", "text/plain")] "Hello, Web!"
+app request respond =
+  case pathInfo request of
+    [] -> appRoot request respond
+    ["hello"] -> appHello request respond
+    ["hello", name] -> appHelloName name request respond
+    _ -> appNotFound request respond
+
+appRoot :: Application
+appRoot _ respond =
+  respond $ responseLBS ok200 [("Content-Type", "text/plain")] "Hello, Web!"
+
+appHello :: Application
+appHello _ respond =
+  respond $ responseLBS ok200 [("Content-Type", "text/plain")] "Hello!"
+
+appHelloName :: Text -> Application
+appHelloName name _ respond =
+  respond $
+  responseLBS
+    ok200
+    [("Content-Type", "text/plain")]
+    ("Hello, " <> (fromStrict $ encodeUtf8 name) <> "!")
+
+appNotFound :: Application
+appNotFound _ respond =
+  respond $ responseLBS notFound404 [("Content-Type", "text/plain")] "NotFound"
 
 main :: IO ()
 main = run 8080 app
