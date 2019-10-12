bd5da1105c8aa5201bcd4d6539e44f32d073a775
Goryudyuma <umaretekyoumade@gmail.com>
2019-10-12 22:47:53 +0900

    HTTP Method

--- 06-wai/wai-test/app/Main.hs
+++ 06-wai/wai-test/app/Main.hs
@@ -4,18 +4,34 @@ import Control.Monad (join)
 import Data.ByteString.Lazy (fromStrict)
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
-import Network.HTTP.Types (notFound404, ok200)
-import Network.Wai (Application, pathInfo, queryString, responseLBS)
+import Network.HTTP.Types
+  ( methodDelete
+  , methodGet
+  , methodNotAllowed405
+  , notFound404
+  , ok200
+  )
+import Network.Wai
+  ( Application
+  , pathInfo
+  , queryString
+  , requestMethod
+  , responseLBS
+  )
 import Network.Wai.Handler.Warp (run)
 
 app :: Application
 app request respond =
-  let mname = join $lookup "name" $ queryString request
+  let method = requestMethod request
    in respond $
-      responseLBS ok200 [("Content-Type", "text/plain")] $
-      case mname of
-        Just name -> "Hello, " <> (fromStrict name) <> "!"
-        Nothing -> "Hello!"
+      if method == methodGet
+        then responseLBS ok200 [("Content-Type", "text/plain")] $ "Hello!"
+        else if method == methodDelete
+               then responseLBS ok200 [("Content-Type", "text/plain")] $ "Bye!"
+               else responseLBS
+                      methodNotAllowed405
+                      [("Content-Type", "text/plain")] $
+                    "Method Not Aloowed"
 
 main :: IO ()
 main = run 8080 app
